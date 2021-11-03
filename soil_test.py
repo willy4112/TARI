89349ed4c01daedc689ec1d19f7ee06f951bea6a
willy4112 <93559065+willy4112@users.noreply.github.com>
2021-11-03 14:24:05 +0800

    Update soil_test.py

--- soil_test.py
+++ soil_test.py
@@ -8,16 +8,24 @@ Created on Tue Nov  2 09:47:43 2021
 import os
 import numpy as np
 import pandas as pd
+import re
+
 
 ## 找到所有的檔名 ###
 # 取得名稱
 L = os.listdir(r'C:\Users\acer\Desktop\soil test\Soil')
 # 建立要複製的檔名
 L1 = []
+L2 = []
 filepath = []
 for i in range(0,547):
     a = L[i]+'.soi'
+    c = L[i]
+    # 取得編號
+    d = re.findall(r'-?\d+\.?\d*', c)
+    d = d[0]
     L1.append(a)
+    L2.append(d)
     f1 = 'C:\\Users\\acer\\Desktop\\soil test\\Soil\\'
     f1 = f1+L[i]+'\\'+a
     filepath.append(f1)
@@ -38,18 +46,26 @@ for j in range(0,547):
     length.append(lines)
     b = length[j]
     if b <=0:
-        b = 'Error'
+        b = 'Not done'
     elif b < 18:
-        b = 'Lack'
+        b = 'Data missing'
     else:
         b = 'OK'
     stay.append(b)
 
 
 ### 將資料串聯 ###
-name = ['filename','data length','stay']
-data = np.array([L1,length,stay])
+name = ['No','filename','data length','stay']
+data = np.array([L2,L1,length,stay])
 data = data.T
 df = pd.DataFrame(data,columns=name)
+# 檢查資料型態
+# df.dtypes
+# 轉換資料型態
+df['No'] = df['No'].astype('int')
+df['data length'] = df['data length'].astype('int')
+df = df.sort_values(by='No',ascending=True)
+df.reset_index(inplace=True)
+df = df.drop(['index'],axis=1)
 df.to_csv(r'C:\Users\acer\Desktop\soil test\土壤物理化學分析數據_測試結果.csv')
 
