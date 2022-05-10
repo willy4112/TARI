22042803740ef4b0a8ac9ff23327b2e7e1c1d982
willy4112 <93559065+willy4112@users.noreply.github.com>
2022-05-10 09:07:38 +0800

    Create Headwall data rename to data.bil.py

---
+++ Headwall data rename to data.bil.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 14:37:13 2022
+
+@author: Chia-Wei Wang
+"""
+
+import glob
+import os
+
+# 輸入區
+# 設定要執行之資料夾的資料夾路徑，最後請用\\結尾
+filename = r'K:\headwall 600-1700 nm\\'
+find = 'data'
+
+# 執行區
+path = filename
+# 純粹好看不想結尾有\\，而是\
+path = path[:-1]
+# 取得資料夾下的個別資料夾名稱，也就是得到檔案名
+fileList = os.listdir(path)
+
+# 把 data變成 檔名.bil
+for i in range(len(fileList)):
+    # 取得要命名之檔名
+    name = fileList[i]
+    
+    # 取得要改寫檔明知檔案
+    L = glob.glob(path+name+'\\'+find)
+    
+    for j in range(len(L)):
+        # 原先的完整路徑
+        oldname = L[j]
+        # 取得副檔名
+        (file,extension) = os.path.splitext(oldname)
+        # 命名新黨名與路徑
+        newname = path+name+'\\'+name+'.bil'+extension
+        # 改寫
+        os.rename(oldname,newname)
+
+# 把 data.hdr變成 檔名.bil.hdr
+for i in range(len(fileList)):
+    # 取得要命名之檔名
+    name = fileList[i]
+    
+    # 取得要改寫檔明知檔案
+    L = glob.glob(path+name+'\\'+find+'*.*')
+    
+    for j in range(len(L)):
+        # 原先的完整路徑
+        oldname = L[j]
+        # 取得副檔名
+        (file,extension) = os.path.splitext(oldname)
+        # 命名新黨名與路徑
+        newname = path+name+'\\'+name+'.bil'+extension
+        # 改寫
+        os.rename(oldname,newname)
