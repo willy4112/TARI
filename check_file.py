aa115e452254440723cf102ccf0604f185ef8488
willy4112 <93559065+willy4112@users.noreply.github.com>
2022-06-08 16:54:23 +0800

    Update check_file.py
    更改0306資料hotDays彙整cell中
    (1) 單年度資料的迴圈修改
    (2) report 以funtion方式執行

--- check_file.py
+++ check_file.py
@@ -21,7 +21,7 @@ list_file.remove('_Draw')
 list_file.remove('_Draw_2D')
 list_file.remove('_Draw_3D')
 list_file.remove('舊程式碼')
-
+list_file.remove('_report')
 
 
 see = []
@@ -66,6 +66,160 @@ for i in list_file:
     num = len(num_file)
     L.append([i,num])
 df = pd.DataFrame(L)
+
+#%% 0306資料hotDays彙整
+
+import os
+import glob
+import pandas as pd
+
+# 行政院農業委員會資料開放平台>雜糧生產概況
+# 109,全年,食用玉米
+# https://data.coa.gov.tw/open_search.aspx?id=8v9pJZ858uOz
+
+# 建立目標清單
+list_city = {'Taoyuan':['楊梅區','中壢區','平鎮區'],
+             'Hsinchu':['竹北市','關西鎮','竹東鎮','橫山鄉','新豐鄉'],
+             'Miaoli':['大湖鄉','通霄鎮','後龍鎮','公館鄉'],
+             'Taichung':['大里區','沙鹿區','大雅區','大肚區','神岡區','豐原區','清水區','后里區'],
+             'Changhua':['芳苑鄉','大城鄉','埔鹽鄉','溪州鄉','福興鄉','二林鎮','彰化市','埤頭鄉','溪湖鎮','和美鎮'],
+             'Yunlin':['虎尾鎮','元長鄉','土庫鎮','東勢鄉','褒忠鄉','莿桐鄉','四湖鄉','口湖鄉','臺西鄉','水林鄉','林內鄉'],
+             'Chiayi':['六腳鄉','太保市','水上鄉','新港鄉','義竹鄉','鹿草鄉'],
+             'Tainan':['安南區','安定區','歸仁區','新市區','西港區','永康區','仁德區','山上區'],
+             'Kaohsiung':['大寮區','永安區','美濃區','路竹區','湖內區','橋頭區','梓官區','岡山區'],
+             'Pingtung':['高樹鄉','車城鄉','鹽埔鄉','新園鄉','牡丹鄉','萬丹鄉','恆春鎮']
+                 }
+
+list_name = list(list_city)
+
+# =============================================================================
+# 請輸入要查找的區域與年分範圍
+name = 'Taoyuan'
+list_name = list_city[name]
+y_min = 2026    # 2026
+y_max = 2056    # 2056
+# =============================================================================
+
+# 匯入城鎮資料
+city = r'E:\臺灣歷史氣候重建資料_5公里\grid_5km_town2.csv'
+df_city = pd.read_csv(city,encoding='BIG5')
+# 篩選出目標地點
+list_city = df_city[df_city['TOWNNAME'].isin(list_name)]
+# 建立index list
+number = list_city.index
+
+# 限制季節
+season ='1_spring'
+# 選擇相同季節之檔案
+list_all = glob.glob(fr'E:\Maize stage future\*\{season}\*')
+# 檔名包含0306
+list_all = [f for f in list_all if '0306' in f ]
+# 檔案名包含rcp26之檔案
+rcp = ["rcp26","rcp45","rcp60","rcp85"]
+# rcp = ["rcp26",]
+# 建立年份清單
+Year = [i for i in range(y_min,y_max)]
+
+for k in rcp:
+    rcp = k
+    # 篩選RCP
+    list_all_1 = [f for f in list_all if rcp in f ]
+    # 篩選年分
+    for j in range(0,len(Year)):
+        list_all_2 = [f for f in list_all_1 if str(Year[j]) == f[-12:-8] ]
+        # 取hotdays
+        df1 = pd.read_csv(list_all_2[0],usecols = [0,1,2,6],encoding='big5').iloc[number,:]
+        df1.columns = ['ID','lon','lat',list_all_2[0].split('\\')[2]]
+        df1_1 = df1
+        for i in range(1,len(list_all_2)):
+            df1 = pd.read_csv(list_all_2[i],usecols = [0,1,2,6],encoding='big5').iloc[number,:]
+            df1.columns = ['ID','lon','lat',list_all_2[i].split('\\')[2]]
+            df1_1 = pd.concat([df1_1,df1.iloc[:,-1]],axis=1)
+        # 缺值補-1
+        df1_1 = df1_1.fillna(-1)
+        year = Year[j]
+        globals()[f'data_{year}']=df1_1
+        print(f'<<<<< Analyze {rcp} {year} , done! >>>>>')
+    # 將所有年份合併
+    L = [globals()[f'data_{n}'] for n in Year]
+    data = pd.concat(L,keys=Year)
+    globals()[f'data_{rcp}']=data
+    # 儲存檔案
+    globals()[f'data_{rcp}'].to_csv(fr'E:\Maize stage future\_report\{name}_data_{rcp}.csv')
+    
+    # # =====刪除用不到的變數=====
+    for i in Year:
+        del globals()[f'data_{i}']
+# =====刪除用不到的變數=====
+del df1,df1_1,i,j,k,L,rcp,year
+
+
+def report(rcp,num1,num2):
+    # 建立熱逆境日數清單
+    L = [i for i in range(num1,num2+1)]
+    # 建立檔名
+    a = f'times_{rcp}'
+    # 連結到data
+    b = globals()[f'data_{rcp}']
+    # 建立檔名
+    c = f'table_{rcp}'
+    # 開始計算各熱逆境日數
+    for i in L:
+        hotdays = i
+        # 判定是否大於熱逆境日數
+        a = b[b.columns[3:]] > hotdays
+        # 轉換為數字
+        a = a+0
+        # 將各模式結果平均
+        a[f'times-{hotdays}D (%)'] = a.mean(axis=1)*100
+        # 以年分為群組平均
+        a = round(a.groupby(level=[0]).mean(),2)
+        globals()[f'times_{rcp}_{hotdays}'] = a[f'times-{hotdays}D (%)']
+    # 合併不同各熱逆境日數結果
+    L = [globals()[f'times_{rcp}_{n}'] for n in range(num1,num2+1)]
+    globals()[f'report_times_{rcp}'] = pd.concat(L,axis=1)
+    # 連結到f'report_times_{rcp}'
+    d = globals()[f'report_times_{rcp}']
+    
+    # 判定數值>=0
+    data_mask = b >=0
+    # 將缺值得-1改為0
+    b = b*data_mask
+    # 將data以年份為群組加總
+    c = b.iloc[:,3:].groupby(level=[0]).sum()
+    # 命'sum (days)'欄為加總結果
+    c['sum (days)'] = c.sum(axis=1)
+    
+    
+    # 合併times宇加總資料
+    globals()[f'report_{rcp}'] = pd.concat([d,c['sum (days)']],axis=1)
+    
+    # 儲存檔案
+    globals()[f'report_{rcp}'].to_csv(fr'E:\Maize stage future\_report\{name}_report_{rcp}.csv')
+    
+    # =====刪除用不到的變數=====
+    for i in range(num1,num2+1):
+        del globals()[f'times_{rcp}_{i}']
+    del L,i
+    del globals()[f'report_times_{rcp}']
+
+report('rcp26',1,11)
+report('rcp45',1,11)
+report('rcp60',1,11)
+report('rcp85',1,11)
+
+
+# 資料散布情況
+# table_rcp26 = data_rcp26.iloc[:,3:].groupby(level=[0]).sum()
+# t_sum = table_rcp26.sum(axis=1)
+# t_max = table_rcp26.max(axis=1)
+# t_min = table_rcp26.min(axis=1)
+# t_25 = table_rcp26.quantile(.25,axis=1)
+# t_50 = table_rcp26.quantile(.50,axis=1)
+# t_75 = table_rcp26.quantile(.75,axis=1)
+# t_all = pd.concat([t_sum,t_max,t_min,t_25,t_50,t_75],axis=1)
+# t_all.columns = ['sum','man','min','25%','50%','75%']
+
 
 #%% 歷史資料彙整
 
